161925e066cf6538996fc49753ae3ed376bbaad1
Tom Buchok <tom@buchok.com>
2014-02-20 09:24:39 -0500

    adds error handler for easier error resiliency

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ var init = function() {
   var server = http.createServer(function(req, res) {
     fs.createReadStream(process.cwd() + '/src/email.jade')
       .pipe(new ComposeNew)
+      .on('error', function(err) {
+        res.end(err.message);
+      })
       .pipe(res)
     ;
   }).listen(3000);
